4565e360f6958c0d78c18cac20da2a01b3e4c11f
hasbihakiki2 <87002122+hasbihakiki2@users.noreply.github.com>
2021-07-12 00:06:55 +0700

    Tugas Quiz Day 1

---
+++ Pertemuan-1/tugas nomor 8.js
@@ -0,0 +1,14 @@
+var k=25;
+var temp={};
+var pos = ["123456789101112"]
+function deretNegation(k){
+    var pos=12;
+    var neg=-14;
+    for (var i=-1; i>neg; i--){
+        console.log(i)
+    }
+    for (var j=0; j<=pos; j++){
+        console.log(j)
+    }
+}
+console.log(deretNegation(25))
