5b0827d0320051322fdffd2dfc74cc06d2dc6894
hasbihakiki2 <87002122+hasbihakiki2@users.noreply.github.com>
2021-07-12 12:39:07 +0700

    Tugas Quiz Day 1

--- Pertemuan-1/tugas nomor 8.js
+++ Pertemuan-1/tugas nomor 8.js
@@ -1,14 +1,16 @@
 var k=25;
-var temp={};
-var pos = ["123456789101112"]
+var x = 0
+var angkaTemp=1;
 function deretNegation(k){
-    var pos=12;
-    var neg=-14;
-    for (var i=-1; i>neg; i--){
-        console.log(i)
-    }
-    for (var j=0; j<=pos; j++){
-        console.log(j)
+    for (var i= 0; i<k; i++){
+        if( i%2 === 0){
+            x = x+angkaTemp*-1;
+            console.log(x)
+        }else{
+            x=x+angkaTemp*1;
+            console.log(x)
+        }
+        angkaTemp++
     }
 }
 console.log(deretNegation(25))
